8d57dd20f38a8cd6ed60b69c4d036762d68b204d
João Gabriel <joaogabrielsr39@gmail.com>
2022-12-19 11:32:50 -0300

    modified todo project

--- Autoria-Propria/Todo-List/scripts.js
+++ Autoria-Propria/Todo-List/scripts.js
@@ -1,34 +1,77 @@
-const tasks = JSON.parse(localStorage.getItem('lista_todos')) || ['Estudar HTML e CSS', 'Aprender Javascript', 'Arrumar Casa'],
-      list = document.querySelector('.list'),
-      todoAddField = document.getElementsByTagName('input'),
-      addButton = document.getElementsByTagName('span')[0]
+const list = JSON.parse(localStorage.getItem('lista_todos')) || []
+const inputTodo = document.querySelector('#input')
+const btnAdd = document.querySelector('#input-button')
+const btnEdit = document.querySelector('#edit')
+const btnDelete = document.querySelector('#delete')
+const listTodo = document.querySelector('#list')
 
-const renderList = () => {
-    list.innerHTML = ''
-
-    for (let i = 0; i < tasks.length; i++) {
-        list.innerHTML += `<li><p>${tasks[i]}</p><a><span class='material-symbols-outlined' id='delete'>delete</span></a> <a><span class='material-symbols-outlined' id='edit'>edit</span></a></li>`
-    }
-}
-
-const addTodo = () => {
-    if (todoAddField.value == '') {
-        window.alert('Você não digitou nenhuma tarefa, por favor digite-a')
-        return false
+// ADD TODO
+function addTodo() {
+    if (inputTodo.value == '') {
+        window.alert('[ERRO] Nenhuma tarefa digitada!')
     } else {
-        let task = todoAddField.value
-        console.log(task)
-        tasks.push(task)
-        todoAddField.value = ''
-        renderList()
-        saveData()
+        list.push(inputTodo.value)
+        save()
+        renderTodo()
+        inputTodo.value = ''
     }
 }
 
-const saveData = () => {
-    localStorage.setItem('lista_todos', JSON.stringify(todos))
+// RENDER TODO
+function renderTodo() {
+    listTodo.innerHTML = ''
+
+    if (list.length === 0) {
+        listTodo.innerHTML += '<li>Nenhuma tarefa adicionada, adicione algumas!</li>'
+    } else {
+        for (let i = 0; i < list.length; i++) {
+            let position = list.indexOf(list[i])
+
+            listTodo.innerHTML += `<li>${list[i]} <button id="edit" onclick="editTodo(${position})"><span class='material-symbols-outlined' id='edit'>edit</span></button><button id="delete" onclick="deleteTodo(${position})"><span class='material-symbols-outlined' id='delete'>delete</span> </button></li>`
+        }
+    }    
 }
 
-addButton.addEventListener('click', addTodo)
+// SAVE TODO ON LOCALSTORAGE
+function save() {
+    localStorage.setItem('lista_todos', JSON.stringify(list))
+}
 
-renderList()
+// DELETE TODO
+function deleteTodo(position) {
+    // deletePopUp()
+
+    list.splice(position, 1)
+    
+    save()
+    renderTodo()
+}
+
+// EDIT TODO
+function editTodo(position) {
+    // editPopUp()
+    var newTodo = window.prompt('Digite a tarefa')
+
+    list[position] = newTodo
+
+    save()
+    renderTodo()
+}
+
+// TODO: FINALIZAR OS POP-UP
+// EDIT TODO POP UP WINDOW
+// function editPopUp() {
+//     var popUpWindow = window.open('edit-pop-up.html', 'editTodo', 'width=500px, height=350px')
+
+//     // POP-UP EDIT
+//     popUpWindow.document.head = '<link rel="stylesheet" href="pop-up.css">'
+//     popUpWindow.document.write(`<h1>Editar Tarefa</h1>`)
+// }
+
+// DELETE TODO POP UP WINDOW
+// function deletePopUp() {
+//     var popUpWindow = window.open('edit-pop-up.html', 'editTodo', 'width=500px, height=350px')
+// }
+
+//  CALLING FUNCTIONS
+renderTodo()
